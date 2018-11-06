14a21236e1c589ec816420573944cad79effe79d
Brighter <Kritsada.K@student.chula.ac.th>
2018-11-06 15:10:17 +0700

    GG WP

--- botpush.php
+++ botpush.php
@@ -13,7 +13,7 @@ $pushID = 'U0012e35a00ba9eec103e62f76e664271';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello world');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('Hello สวัสดีครับ');
 $response = $bot->pushMessage($pushID, $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
