aedba1c09f0cafa44fd13a9121dbb862949323b9
Brighter <Kritsada.K@student.chula.ac.th>
2018-11-06 16:03:36 +0700

    add code boardCast

--- boardcast.php
+++ boardcast.php
@@ -1,1 +1,54 @@
 
+<?php
+require "vendor/autoload.php";
+require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
+$access_token = 'ubyTb7feZK6Tq12qODF+PF1oYpyfYfUvst+rfE6qXCX0h+ApmjgCZUPrDE79lfF2e2taLkmVFLpNsGqk9aeLx7Fi23v6KzVpfBJfwD/wnxkwjfUCF9q8Wszn2zIXQmdbHyIvpkf2iIrGrX601724qAdB04t89/1O/w1cDnyilFU=';
+// Get POST body content
+$content = file_get_contents('php://input');
+// Parse JSON
+$events = json_decode($content, true);
+// Validate parsed JSON data
+if (!is_null($events['events'])) {
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		//if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' || $event['type'] == 'follow') {
+			// Get text sent
+			if($event['message']['type'] == 'text') {
+				if($event['message']['text'] == "are you ?") {
+					$text = "Good";
+				}
+				else {
+					$text = "i can't answer you.";
+				}
+			}
+			//$text = $event['source']['userId'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'https://api.line.me/v2/bot/message/reply';
+			$data = [
+				'replyToken' => $replyToken,
+				'messages' => [$messages],
+			];
+			$post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			$ch = curl_init($url);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			echo $result . "\r\n";
+		}
+	}
+}
+echo "OK";
